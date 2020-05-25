2e93c5207e2da1fb856649ae32264942f4525d66
Ezhil Vendhan <uibuddha@gmail.com>
2020-05-25 19:09:14 +0800

    Add P04 - Mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0
+
+@R0
+D=M
+@STEP
+D;JGT
+
+@END
+0;JMP
+
+(STEP)
+  @R2
+  D=M
+
+  @R1
+  D=D+M
+
+  @R2
+  M=D
+
+  @R0
+  D=M-1
+  M=D
+
+  @STEP
+  D;JGT
+
+(END)
+  @END
+  0;JMP
